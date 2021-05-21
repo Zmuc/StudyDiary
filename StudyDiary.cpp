640d4caf578043cb403d81ef9e0afee1758d65cb
Zmuc <81313946+Zmuc@users.noreply.github.com>
2021-05-21 07:10:13 +0000

    cpp: add pair and unordered_map
    cpp: add pair and unordered_map

--- StudyDiary.cpp
+++ StudyDiary.cpp
@@ -1,16 +1,39 @@
 /* C++ 特性 */
+  
 // C++ 11：Range-based for loops
 vector<int> vec;
 for (int i : vec) { }
 //循环执行容器vec中的所有数据
 
 
-/* 排序 */
+/* STL容器 */
+
+// unordered_map：唯一键的键/值对元素hash表，无序
+template < class Key,
+           class T,
+           class Hash = hash<Key>,
+           class Pred = equal_to<Key>,
+           class Alloc = allocator< pair<const Key,T> > > class unordered_map;
+//Key：键
+//T：值
+//Hash，Pred，Alloc：一般不用
+
+
+/* 数据类型 */
+
+// pair：将2个数据组合成1组数据
+template < class T1,
+           class T2 > struct pair；
+//T1，T2：int，string，vector<int>等数据类型
+
+
+/* 算法 */
 
 // sort：排序
 void sort (first_pointer, first_pointer+n, cmp);
 //first_pointer：即数组，链表，队列，容器等首地址，长度为n；
 //cmp：即排序算法，可自定义，less<data-type>()升序，greater<data-type>()降序，默认升序。
+
 
 /* 内存管理 */
 
